8bbacb6a9b13738fc68bf56e1e784fa322b1f25f
tinghung.wang[王庭歡] <tinghung.wang@tinghungwang.local>
2020-05-14 10:07:32 +0800

    新增bootstrap

---
+++ Bootstrap/src/entryHello.js
@@ -0,0 +1,9 @@
+import HelloButton from './hello/hello.js'
+import Handing from './common/handing.js'
+import _ from 'lodash'
+
+const handing = new Handing();
+handing.render(_.upperFirst('hello'));
+
+const helloButton = new HelloButton();
+helloButton.render();
