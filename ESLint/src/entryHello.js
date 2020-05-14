9703fd768270c364d0e0d886490ba14e7e2c4029
tinghung.wang[王庭歡] <tinghung.wang@tinghungwang.local>
2020-05-14 10:27:20 +0800

    新增eslint

--- ESLint/src/entryHello.js
+++ ESLint/src/entryHello.js
@@ -1,8 +1,6 @@
 import HelloButton from './hello/hello.js'
 import Handing from './common/handing.js'
 import _ from 'lodash'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 const handing = new Handing();
 handing.render(_.upperFirst('hello'));
